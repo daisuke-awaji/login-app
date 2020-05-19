958e954d987d362f9117d0c1a3945c8467daaa13
daisuke-awaji <daisuke.awaji@i.softbank.jp>
2020-05-19 23:25:44 +0900

    format grid layout in home page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,28 +3,21 @@ import { makeStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 // import CardContent from '@material-ui/core/CardContent'
 import Typography from '@material-ui/core/Typography'
-import { Grid, CardActionArea, Container, Avatar } from '@material-ui/core'
+import { Grid, Avatar, Container } from '@material-ui/core'
 import { useHistory } from 'react-router-dom'
 import PeopleIcon from '@material-ui/icons/People'
 const useStyles = makeStyles((theme) => ({
   actionArea: {
-    display: 'block',
-    width: '20vw',
-    transitionDuration: '0.3s',
-    height: '20vw',
-    padding: theme.spacing(2),
-  },
-  title: {
-    fontSize: 28,
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
+    width: '120px',
+    height: '120px',
+    transitionDuration: '0.3s',
   },
   avatar: {
-    margin: theme.spacing(3),
     backgroundColor: theme.palette.primary.main,
-    width: '80px',
-    height: '80px',
+    margin: 'auto',
   },
 }))
 
@@ -35,29 +28,55 @@ export const Home = () => {
     history.replace('/users')
   }
 
+  const CardOne = () => (
+    <Container component="main" maxWidth="xs">
+      <Card className={classes.actionArea} onClick={handleClick}>
+        <Avatar className={classes.avatar}>
+          <PeopleIcon fontSize="large" />
+        </Avatar>
+        <Typography color="textSecondary" gutterBottom>
+          Users
+        </Typography>
+      </Card>
+    </Container>
+  )
+
   return (
     <div>
       <Typography variant="h2" gutterBottom>
         Home
       </Typography>
-      <Grid
-        container
-        direction="row"
-        justify="flex-start"
-        alignItems="flex-start"
-      >
-        <Card>
-          <CardActionArea className={classes.actionArea} onClick={handleClick}>
-            <Container component="main" maxWidth="xs" className={classes.title}>
-              <Avatar className={classes.avatar}>
-                <PeopleIcon fontSize="large" />
-              </Avatar>
-              <Typography color="textSecondary" gutterBottom>
-                Users
-              </Typography>
-            </Container>
-          </CardActionArea>
-        </Card>
+      <Grid container direction="row" justify="flex-start" spacing={1}>
+        <Grid item>
+          <CardOne />
+        </Grid>
+        <Grid item>
+          <CardOne />
+        </Grid>
+        <Grid item>
+          <CardOne />
+        </Grid>
+        <Grid item>
+          <CardOne />
+        </Grid>
+        <Grid item>
+          <CardOne />
+        </Grid>
+        <Grid item>
+          <CardOne />
+        </Grid>
+        <Grid item>
+          <CardOne />
+        </Grid>
+        <Grid item>
+          <CardOne />
+        </Grid>
+        <Grid item>
+          <CardOne />
+        </Grid>
+        <Grid item>
+          <CardOne />
+        </Grid>
       </Grid>
     </div>
   )
