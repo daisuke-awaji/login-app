24ca6653f35454054ad4674c568a0cb37627ed29
awaji-daisuke <awaji_daisuke@intec.co.jp>
2020-05-25 10:13:14 +0900

    add dummy user

--- src/apis/users.ts
+++ src/apis/users.ts
@@ -1,8 +1,7 @@
-// mock
-
 import { IUser } from 'components/users/IUser'
 import { sleep } from './utils'
 
+// mock
 export const users: IUser[] = [
   {
     id: '0342a27b-e1f8-456b-8bc8-8e7cff1f4d9a',
@@ -16,11 +15,46 @@ export const users: IUser[] = [
   },
   {
     id: '101dfbc1-5384-44f2-8628-f2399b46fdd6',
-    name: 'Muhammed Erdem',
+    name: 'Bob',
     email: 'userb@email.com',
     type: 'default',
     img:
       'https://res.cloudinary.com/muhammederdem/image/upload/v1537638518/Ba%C5%9Fl%C4%B1ks%C4%B1z-1.jpg',
+  },
+  {
+    id: 'b1a79255-caf8-4bd2-b238-c784454fb5b0',
+    name: 'c',
+    email: 'userc@email.com',
+    type: 'default',
+    img: '',
+  },
+  {
+    id: 'b56695d9-2e89-4a0a-9434-ebfac97bc778',
+    name: 'd',
+    email: 'userd@email.com',
+    type: 'default',
+    img: '',
+  },
+  {
+    id: 'fca20c64-4c30-4cca-bd57-71fe091eb324',
+    name: 'e',
+    email: 'usere@email.com',
+    type: 'default',
+    img: '',
+  },
+  {
+    id: '229625dd-a2cc-41b9-bf4b-98cb32d07e2d',
+    name: 'f',
+    email: 'userf@email.com',
+    type: 'default',
+    img: '',
+  },
+  {
+    id: 'e2506be7-37a3-4228-b83e-4e345540d69e',
+    name: 'g',
+    email: 'userg@email.com',
+    type: 'default',
+    img: '',
   },
 ]
 
