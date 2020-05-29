d864766a09fdc0dc2d6903ec0244669c47d62d20
awaji-daisuke <awaji_daisuke@intec.co.jp>
2020-05-29 17:17:59 +0900

    redux dev tool and add logger

--- src/reducers/store.tsx
+++ src/reducers/store.tsx
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import { combineReducers } from 'redux'
 import auth from './authenticate'
 import thunk from 'redux-thunk'
@@ -7,11 +7,28 @@ const reducer = combineReducers({
   authenticatedUser: auth,
 })
 
-// TypeScript で Redux Dev Tools を使用する
-// const composeEnhancers =
-// (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+/**
+ * Logs all actions and states after they are dispatched.
+ * もうこれでいいや
+ */
+const logger = (store: any) => (next: (arg0: any) => any) => (action: {
+  type: any
+}) => {
+  console.group(action.type)
+  console.info('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  console.groupEnd()
+  return result
+}
 
-export default createStore(
-  reducer /* preloadedState, */,
-  applyMiddleware(thunk),
+// TypeScript で Redux Dev Tools を使用する
+const composeEnhancers =
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  reducer,
+  composeEnhancers(applyMiddleware(thunk, logger)),
 )
+
+export default store
